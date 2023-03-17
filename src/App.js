ea2fc2c39a2f7ac063312ce9ba05418fe38e69a0
rohinsha <r2hinsha@gmail.com>
2023-03-17 18:12:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./styles.css";
+import { useEffect, useState } from "react";
+const countries = [
+  { name: "India", value: "IN", cities: ["Delhi", "Mumbai"] },
+  { name: "Pak", value: "PK", cities: ["Lahore", "Karachi"] },
+  { name: "Bangladesh", value: "BG", cities: ["Dhaka", "Chittagong"] }
+];
+
+export default function App() {
+  // state to store the value of the country
+  const [country, setCountry] = useState(0);
+
+  // useEffect(() => {
+  //   console.log(country[0]);
+  //   console.log(country[1]);
+  // }, [country]);
+
+  return (
+    <div className="App">
+      {/* 1st DropDown */}
+      <select
+        value={country}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setCountry([e.target.value]);
+        }}
+      >
+        {countries.map((item, index) => {
+          return (
+            <option key={index} value={index}>
+              {item.name}
+            </option>
+          );
+        })}
+      </select>
+
+      {/* 2nd DropDown */}
+
+      <select>
+        {countries[country] &&
+          countries[country].cities.map((item, index) => {
+            return <option value={index}>{item}</option>;
+          })}
+      </select>
+    </div>
+  );
+}
